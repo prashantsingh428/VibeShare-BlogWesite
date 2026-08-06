65a3fff24706190e50fe4345f9d72fea89036fa8
aigrowthexa <aigrowthexa@gmail.com>
2026-08-06 18:53:04 +0530

    Fix Vercel views path: use absolute path for views and public directories

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const upload = require("./config/multerconfig");
 
 /* ===================== CONFIG ===================== */
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
